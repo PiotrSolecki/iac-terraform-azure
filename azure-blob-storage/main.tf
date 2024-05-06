4eaabfcfd0120c9912fcad60dd8f4a68a5f6928e
PiotrSolecki <127014084+PiotrSolecki@users.noreply.github.com>
2024-05-06 18:37:46 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
 resource "azurerm_storage_account" "example" {
